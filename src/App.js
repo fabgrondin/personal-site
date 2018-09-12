8621841f3f111ffc6edcf16428e6f4f07f98165e
fabgrondin <bart.rapia@gmail.com>
2018-09-12 15:00:12 +0200

    Add dynamic router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import ToggleMenu from './ToggleMenu.js'
+
 import logo from './logo.svg';
 import './App.css';
+
+const Welcome = () => (
+  <h1>Bienvenue</h1>
+)
+
+const Resume = () => (
+  <h1>Mon CV</h1>
+)
+
+const Portfolio = () => (
+  <h1>Mon portfolio</h1>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ToggleMenu />
-        <p id="welcome_text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore neque consectetur dolores, quas est? Iusto animi, facilis eveniet cumque voluptatum sunt beatae nobis, minima temporibus, quod, accusantium quas laborum repellat.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <ToggleMenu />
+
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </div>
+      </Router>
     );
   }
 }
--- src/ToggleMenu.js
+++ src/ToggleMenu.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom'
 import './ToggleMenu.css'
 
 class ToggleMenu extends Component {
@@ -14,9 +15,21 @@ render() {
       <span role="button" id="menu_display" onClick={this.toggleMenu}><i className="fas fa-bars"></i></span>
       {this.state.visible && (
         <ul>
-          <li><i className="fas fa-home"></i></li>
-          <li><i className="fas fa-file"></i></li>
-          <li><i className="fas fa-images"></i></li>
+          <li>
+            <NavLink exact to="/" title="Accueil">
+              <i className="fas fa-home"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/resume" title="Curriculum Vitae">
+              <i className="fas fa-file"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/portfolio" title="Portfolio">
+              <i className="fas fa-images"></i>
+            </NavLink>
+          </li>
         </ul>
       )}
     </nav>
