e9c73aababf0fc2a201d0100bef9e2dabdd97e40
fabgrondin <bart.rapia@gmail.com>
2018-09-20 11:46:02 +0200

    Adjust transitions

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -10,37 +10,60 @@ class Carousel extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      lazyLoad: "progressive"
     };
 
     return (
-      <Slider {...settings}>
-        <div className="slide">
-          <img src="/images/slide1.png" alt=""/>
-          <div className="legend">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus aut tempora quo, quisquam voluptas corrupti a minus, nihil fugit repellat quam recusandae expedita repellendus nam non saepe. Officia, ab, natus.
+      <div id="slider_wrapper">
+        <Slider {...settings}>
+          <div className="slide">
+            <img src="/images/slide1.png" alt=""/>
+            <div className="legend">
+            <h2>Projet Web Agency</h2>
+            <p>
+              Intégration d&rsquo;une maquette en HTML et CSS avec un design responsive.
+            </p>
+            </div>
           </div>
-        </div>
-        <div className="slide">
-          <img src="/images/slide2.png" alt=""/>
-          <div className="legend">Nulla iusto tempora minima cumque magnam provident, dicta facilis quod veritatis impedit distinctio eius illum similique, recusandae aperiam dolorem necessitatibus culpa optio debitis! Sunt iure quas, possimus. Accusantium, sint nihil.
+          <div className="slide">
+            <img src="/images/slide2.png" alt=""/>
+            <div className="legend">
+            <h2>Créer un site wordpress</h2>
+            <p>
+              Création de thème enfant | SEO | Respect des normes d&rsquo;accessibilité WCAG 2.0
+            </p>
+            </div>
           </div>
-        </div>
-        <div className="slide">
-          <img src="/images/slide3.png" alt=""/>
-          <div className="legend">Dolorem consequatur itaque unde ut magni! Alias velit hic, culpa reprehenderit maiores eaque! Blanditiis quam consectetur at. Incidunt optio, praesentium nulla, ipsum, a totam, veritatis inventore cupiditate tempore repellendus est.
+          <div className="slide">
+            <img src="/images/slide3.png" alt=""/>
+            <div className="legend">
+            <h2>Carte interactive de location de vélos</h2>
+            <p>
+              POO JavaScript | API Google Map | API JCDecaux | API Web Storage
+            </p>
+            </div>
           </div>
-        </div>
-        <div className="slide">
-          <img src="/images/slide4.png" alt=""/>
-          <div className="legend">Dolore vitae sapiente sed necessitatibus pariatur veniam distinctio error minus! Accusantium hic praesentium eum, voluptatem, aut excepturi repellendus asperiores. Assumenda expedita rem architecto provident ipsam, consequuntur deserunt magni officia quaerat.
+          <div className="slide">
+            <img src="/images/slide4.png" alt=""/>
+            <div className="legend">
+            <h2>Un blog pour un écrivain</h2>
+            <p>
+              Application PHP et MySQL | POO | MVC | Bootstrap
+            </p>
+            </div>
           </div>
-        </div>
-        <div className="slide">
-          <img src="/images/slide5.png" alt=""/>
-          <div className="legend">Numquam placeat eaque voluptates, corrupti cupiditate, non velit temporibus doloremque dolor quidem officiis pariatur porro minus quo adipisci accusamus rerum iste eveniet quia? Impedit quis ab, sed aliquam expedita sint.
+          <div className="slide">
+            <img src="/images/slide5.png" alt=""/>
+            <div className="legend">
+            <h2>Portail d&rsquo;activités pour enfants dans sa ville</h2>
+            <p>
+              Application PHP et MySQL utilisant les frameworks Laravel et Bootstrap
+            </p>
+            </div>
           </div>
-        </div>
-      </Slider>
+        </Slider>
+      </div>
     )
   }
 }
